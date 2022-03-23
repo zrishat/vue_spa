dd3e6741bcbb6fbe322a064fe3a8448dfbb20a20
Rishat Zainullin <r.zainullin@omp.ru>
2022-03-24 01:26:35 +0300

    Add router

---
+++ src/views/Homepage.vue
@@ -0,0 +1,38 @@
+<script setup>
+import SettingsMenu from '../components/SettingsMenu.vue'
+import TheWelcome from '../components/TheWelcome.vue'
+
+</script>
+
+<template>
+  <TheWelcome />
+  <SettingsMenu />
+  <button @click="goPlay">Play</button>
+</template>
+
+
+<script>
+  export default {
+    methods: {
+      goPlay() {
+        this.$router.push('/play')
+      }
+    }
+  }
+</script>
+
+
+<style>
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+input{
+  margin: 10px;
+  display: inline-block;
+}
+</style>
---
+++ src/views/Play.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Supergame</h1>
+  <p>...bla..bla.bla</p>
+  <button @click="goBack">go home</button>
+</template>
+<script>
+  export default {
+    methods: {
+      goBack() {
+        this.$router.push('/')
+      }
+    }
+  }
+</script>
